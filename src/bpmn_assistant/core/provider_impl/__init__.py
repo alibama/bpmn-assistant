f63659128ed0c9086cadfdac238877186942c047
alibama <adp6j@virginia.edu>
2024-11-11 14:57:41 -0500

    Update __init__.py

--- src/bpmn_assistant/core/provider_impl/__init__.py
+++ src/bpmn_assistant/core/provider_impl/__init__.py
@@ -1,9 +1,11 @@
 from .anthropic_provider import AnthropicProvider
 from .google_provider import GoogleProvider
 from .openai_provider import OpenAIProvider
+from .ollama_provider import OllamaProvider
 
 __all__ = [
     "AnthropicProvider",
     "OpenAIProvider",
     "GoogleProvider",
+    "OllamaProvider",
 ]
